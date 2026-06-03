e4b5e656e74fda1c9f7d7752ef40a87946b2f1f1
Artem Zyktin <zyktin.a.a@gmail.com>
2026-06-03 17:20:14 +0800

    Add dot4 expression

---
+++ src/tenvex/expressions/dot4.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "core.h"
+#include "expression.h"
+#include "traits.h"
+#include "concepts.h"
+
+namespace tnvx
+{
+
+template<vec_expr L, vec_expr R>
+struct Dot4 : Expr<Dot4<L, R>>
+{
+	TNVX_INLINE
+	Dot4(const L& TNVX_RESTRICT l, const R& TNVX_RESTRICT r) noexcept;
+
+	[[nodiscard]] TNVX_INLINE
+	vf4 eval() const noexcept;
+
+	[[nodiscard]] TNVX_INLINE
+	operator float() const noexcept;
+
+private:
+	tnvx_ref_or_value_t<L> _l;
+	tnvx_ref_or_value_t<R> _r;
+};
+
+template<vec_expr L, vec_expr R> inline constexpr bool is_scalar_expr<Dot4<L, R>> = true;
+
+template<vec_expr L, vec_expr R>
+[[nodiscard]] TNVX_INLINE
+Dot3<L, R> dot4(const L& l, const R& r) noexcept;
+
+template<vec_expr L, vec_expr R>
+TNVX_INLINE
+Dot3<L, R> dot4(const L& l, const R& r) noexcept
+{
+	return { l, r };
+}
+
+}
+
+#include "dot4_impl.hpp"
---
+++ src/tenvex/expressions/dot4_impl.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "dot4.h"
+
+namespace tnvx
+{
+
+template<vec_expr L, vec_expr R>
+TNVX_INLINE
+Dot4<L, R>::Dot4(const L& TNVX_RESTRICT l, const R& TNVX_RESTRICT r) noexcept
+	: _l(l)
+	, _r(r)
+{
+}
+
+template<vec_expr L, vec_expr R>
+TNVX_INLINE
+vf4 Dot4<L, R>::eval() const noexcept
+{
+	return detail::dot4(_l.eval(), _r.eval());
+}
+
+template<vec_expr L, vec_expr R>
+TNVX_INLINE
+Dot4<L, R>::operator float() const noexcept
+{
+	return detail::to_float(eval());
+}
+
+}
