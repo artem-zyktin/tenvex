3544cb5c51f09132baabcdb43f1679f1083edd25
Artem Zyktin <zyktin.a.a@gmail.com>
2026-06-08 19:34:46 +0800

    Add operations

---
+++ src/tenvex/expressions/operations.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "add.h"
+#include "sub.h"
+#include "mul.h"
+#include "magnitude3.h"
+#include "magnitude3_sq.h"
+#include "dot3.h"
+#include "min.h"
+#include "max.h"
+#include "vec4.h"
+
+
+namespace tnvx
+{
+
+template<vec_expr V, vec_expr Lo, vec_expr Hi>
+[[nodiscard]] TNVX_INLINE
+auto clamp(const V& v, const Lo& lo, const Hi& hi) noexcept;
+
+template<vec_expr V>
+[[nodiscard]] TNVX_INLINE
+auto saturate(const V& v) noexcept;
+
+template<vec_expr A, vec_expr B, scalar_expr T>
+[[nodiscard]] TNVX_INLINE
+auto lerp(const A& a, const B& b, tnvx_ref_or_value_t<T> t) noexcept;
+
+template<vec_expr L, vec_expr R>
+[[nodiscard]] TNVX_INLINE
+auto dist3(const L& l, const R& r) noexcept;
+
+template<vec_expr L, vec_expr R>
+[[nodiscard]] TNVX_INLINE
+auto dist3_sq(const L& l, const R& r) noexcept;
+
+template<vec_expr V, vec_expr N>
+[[nodiscard]] TNVX_INLINE
+auto reflect(const V& v, const N& n) noexcept;
+
+}
+
+#include "operations_impl.hpp"
---
+++ src/tenvex/expressions/operations_impl.hpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "operations.h"
+
+namespace tnvx
+{
+
+template<vec_expr V, vec_expr Lo, vec_expr Hi>
+TNVX_INLINE
+auto clamp(const V& v, const Lo& lo, const Hi& hi) noexcept
+{
+	return min(max(v, lo), hi);
+}
+
+template<vec_expr V>
+TNVX_INLINE
+auto saturate(const V& v) noexcept
+{
+	return clamp(v, vec4{ 0.f,0.f,0.f,0.f }, vec4{ 1.f,1.f,1.f,1.f });
+}
+
+template<vec_expr A, vec_expr B, scalar_expr T>
+TNVX_INLINE
+auto lerp(const A& a, const B& b, tnvx_ref_or_value_t<T> t) noexcept
+{
+	return a + (b - a) * t;
+}
+
+template<vec_expr L, vec_expr R>
+TNVX_INLINE
+auto dist3(const L& l, const R& r) noexcept
+{
+	return magnitude3(l, r);
+}
+
+template<vec_expr L, vec_expr R>
+TNVX_INLINE
+auto dist3_sq(const L& l, const R& r) noexcept
+{
+	return magnitude3_sq(l, r);
+}
+
+template<vec_expr V, vec_expr N>
+TNVX_INLINE
+auto reflect(const V& v, const N& n) noexcept
+{
+	v - n * (dot3(v, n) * 2.0f);
+}
+
+}
