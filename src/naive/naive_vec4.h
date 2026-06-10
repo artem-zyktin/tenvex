ef64b38624803c50549b4e58c2865457341f21d9
Artem Zyktin <zyktin.a.a@gmail.com>
2026-06-11 06:35:29 +0800

    Add naive implementation

--- src/naive/naive_vec4.h
+++ src/naive/naive_vec4.h
@@ -186,4 +186,9 @@ inline bool approx_eq(vec4 a, vec4 b, float eps = 1e-6f) noexcept
 			 v.z() - std::floor(v.z()), v.w() - std::floor(v.w()) };
 }
 
+[[nodiscard]] inline vec4 hadamard(vec4 a, vec4 b) noexcept
+{
+	return { a.x() * b.x(), a.y() * b.y(), a.z() * b.z(), a.w() * b.w() };
+}
+
 }
