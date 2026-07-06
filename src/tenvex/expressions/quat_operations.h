601ce398043f2b0c2af6511fbd7c87e16d55e7cf
Artem Zyktin <zyktin.a.a@gmail.com>
2026-07-06 21:50:07 +0800

    Add slerp

---
+++ src/tenvex/expressions/quat_operations.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "core.h"
+#include "traits.h"
+#include "concepts.h"
+
+#include "quat.h"
+
+namespace tnvx
+{
+
+template<quat_expr A, quat_expr B>
+[[nodiscard]] TNVX_INLINE
+quat slerp(const A& a, const B& b, float t) noexcept;
+
+}
+
+#include "quat_operations_impl.hpp"
