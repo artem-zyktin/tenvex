cd86e1a94e7ee0d93d57b5b5b2f17d32d44dbb73
Artem Zyktin <zyktin.a.a@gmail.com>
2026-07-09 14:04:20 +0800

    Fix - normalize4 expression type

--- src/tenvex/expressions/common/normalize4.h
+++ src/tenvex/expressions/common/normalize4.h
@@ -22,7 +22,8 @@ private:
 	tnvx_ref_or_value_t<E> _e;
 };
 
-template<packed_expr E> inline constexpr bool is_quat_expr<Normalize4<E>> = true;
+template<vec_expr E> inline constexpr bool is_vec_expr<Normalize4<E>> = true;
+template<quat_expr E> inline constexpr bool is_quat_expr<Normalize4<E>> = true;
 
 template<packed_expr E>
 Normalize4<E> normalize4(const E& e) noexcept;
