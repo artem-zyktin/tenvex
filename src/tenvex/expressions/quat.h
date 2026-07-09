8e89884fb7e39d14c34bbe9ace43b0a489d93a66
Artem Zyktin <zyktin.a.a@gmail.com>
2026-07-09 13:34:46 +0800

    Fix - change win path separator

--- src/tenvex/expressions/quat.h
+++ src/tenvex/expressions/quat.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "quat\conjugate.h"
-#include "quat\inverse.h"
-#include "quat\quat.h"
-#include "quat\quat_mul.h"
-#include "quat\quat_operations.h"
-#include "quat\rotate.h"
+#include "quat/conjugate.h"
+#include "quat/inverse.h"
+#include "quat/quat.h"
+#include "quat/quat_mul.h"
+#include "quat/quat_operations.h"
+#include "quat/rotate.h"
